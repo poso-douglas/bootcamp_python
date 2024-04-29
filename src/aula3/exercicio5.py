d8b6f152a6539610b4d7d66d6a7c91d860386c83
Douglas Poso <douglas.poso@gmail.com>
2024-04-29 19:37:02 -0300

    adição do exercicio5

---
+++ src/aula3/exercicio5.py
@@ -0,0 +1,10 @@
+### Exercício 5: Detecção de Anomalias em Dados de Transações
+# Você está trabalhando em um sistema de detecção de fraude e precisa identificar 
+# transações suspeitas. Uma transação é considerada suspeita se o valor for superior 
+# a R$ 10.000 ou se ocorrer fora do horário comercial (antes das 9h ou depois das 18h). 
+# Dada uma transação como `transacao = {'valor': 12000, 'hora': 20}`, verifique se ela é suspeita.
+
+transacao = {'valor': 12000, 'hora': 20}
+
+if transacao['valor'] > 10000 and int(transacao['hora']) < 9 or int(transacao['hora']) > 18:
+    print("Transação suspeita")
