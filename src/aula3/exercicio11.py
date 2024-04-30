1d5bb171ada6b563ffcc07a8c97c80e87e8fc875
Douglas Poso <douglas.poso@gmail.com>
2024-04-29 21:02:15 -0300

    Adicionando o exercicio11

---
+++ src/aula3/exercicio11.py
@@ -0,0 +1,6 @@
+### Exercício 11. Leitura de Dados até Flag
+# Ler dados de entrada até que uma palavra-chave específica ("sair") seja fornecida.
+palavra_chave=""
+while palavra_chave != "sair":
+    palavra_chave = input("Insira uma palavra chave ")
+    print("Digite sair para terminar o programa")
