bd82dc21af3155bb3d52982d2ea00318b6cef56f
Douglas Poso <douglas.poso@gmail.com>
2024-05-01 17:09:55 -0300

    Adicionando desfaio e arquivo json gerado

--- src/aula4/desafio.py
+++ src/aula4/desafio.py
@@ -1,33 +1,42 @@
 # Adicionar type hint
 
 import re
+import json
 
-CONSTANTE_CALC: float = 1000
-nome: str = input("Insira seu nome ")
-while re.search(r'\d',nome) or nome.isspace() or len(nome) ==0:
-    print("Você digitou números no campo nome")
-    nome = input("Insira seu nome ")
+def cadastro_bonus(nome: str, salario: float, bonus: float) -> float:
+    CONSTANTE_CALC: float = 1000
+    while re.search(r'\d',nome) or nome.isspace() or len(nome) ==0:
+        print("Você digitou números no campo nome")
+    while salario <=0:
+        try:
+            if salario <= 0:
+                print('Informe um salário maior que 0')
+        except ValueError as e:
+            print(f"Digite um valor decimal - {e}")
+            salario = 0
 
-salario: float = 0
-while salario <=0:
-    try:
-        salario = float(input("Insira seu salário "))
-        if salario <= 0:
-            print('Informe um salário maior que 0')
-    except ValueError as e:
-        print(f"Digite um valor decimal - {e}")
-        salario = 0
+    while bonus <= 0:
+        try:
+            if bonus <= 0:
+                print('Informe um bonus maior que 0')
+        except ValueError as e:
+            print(f"Digite um valor decimal ")
+            bonus = 0
+    ##################################################################################
+    kpi: float = CONSTANTE_CALC + (salario * (bonus/100))
+    dict_retorno = {
+        "nome":nome,
+        "salario":salario,
+        "bonus":bonus,
+        "kpi":f"{kpi:.2f}"
+    }
 
-bonus: float = 0
-while bonus <= 0:
-    try:
-        bonus = float(input("Insira a porcentagem do bonus recebido "))
-        if bonus <= 0:
-            print('Informe um bonus maior que 0')
-    except ValueError as e:
-        print(f"Digite um valor decimal ")
-        bonus = 0
-##################################################################################
-kpi: float = CONSTANTE_CALC + (salario * (bonus/100))
+    dict_json = json.dumps(dict_retorno)
+    with open(file="cadastro.json",mode="w",encoding="utf-8") as json_write:
+        json_write.write(dict_json)
 
-print(f"Olá {nome}, o seu valor bônus foi de {kpi:.2f}")
+    print(f"Olá {nome}, o seu valor bônus foi de {kpi:.2f}")
+    print(dict_retorno)
+
+
+cadastro_bonus("Joao",17.70,8)
