075ced243a5283c5e4299e2b926572ac254abc92
Douglas Poso <douglas.poso@gmail.com>
2024-05-01 15:24:05 -0300

    adicionando o exercicio 18

---
+++ src/aula4/funcao_exe18.py
@@ -0,0 +1,6 @@
+# Desenvolva uma função que receba uma string como argumento e retorne essa string revertida.
+
+def string_reversa(texto: str) -> str:
+    print(texto[::-1])
+
+string_reversa("atecubanos")
