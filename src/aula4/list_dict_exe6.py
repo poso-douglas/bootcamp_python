60995626780dc10dfdbc5b79d5cc1192641fabb7
Douglas Poso <douglas.poso@gmail.com>
2024-05-01 13:06:18 -0300

    Adicionando exercicio 6 de listas e dicionarios

---
+++ src/aula4/list_dict_exe6.py
@@ -0,0 +1,12 @@
+# Dada uma lista de emails, remover todos os duplicados.
+
+lista_entrada: list = ["d@gmail.com", "a@gmail.com", "d@gmail.com", "z@hotmail.com",]
+lista_deduplicada: list = set(lista_entrada)
+print(lista_deduplicada)
+
+# for mail in lista_entrada:
+#     if mail not in lista_deduplicada:
+#         lista_deduplicada.append(mail)
+
+# print(lista_deduplicada)
+
