1e09669fe447aef8954a339b2e2601c571ff9171
Douglas Poso <douglas.poso@gmail.com>
2024-05-01 14:53:10 -0300

    Adicionando exercicio 16

---
+++ src/aula4/funcao_exe16.py
@@ -0,0 +1,6 @@
+# Escreva uma função que receba uma lista de números e retorne a soma de todos os números
+
+def somar_numeros(lista_num: list) -> int:
+    return sum(lista_num)
+
+print(somar_numeros([10,20,30]))
