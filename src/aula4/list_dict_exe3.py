6dd42a9abd19c2dcc7a1126e63f5d21b63ef5bb5
Douglas Poso <douglas.poso@gmail.com>
2024-05-01 11:45:03 -0300

    Adicionando exercicio 3 de listas e dicionarios

---
+++ src/aula4/list_dict_exe3.py
@@ -0,0 +1,20 @@
+# Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. 
+# Imprima cada informação.
+
+livro1: dict = {
+    "titulo":"O Genio Pessoal",
+    "autor": "Jairo Carlos",
+    "ano": 2008
+}
+
+livro2: dict = {
+    "titulo":"O Genio Pessoal",
+    "autor": "Jairo Carlos",
+    "ano": 2008
+}
+
+lista_livros: list =[]
+lista_livros.append(livro1)
+lista_livros.append(livro2)
+
+print(lista_livros)
