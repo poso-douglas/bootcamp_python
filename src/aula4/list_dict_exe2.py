af4ab4ac9e38c3e0db824dae70f77428615862e8
Douglas Poso <douglas.poso@gmail.com>
2024-05-01 11:40:02 -0300

    Adicionando exercicio 2 de listas e dicionarios

---
+++ src/aula4/list_dict_exe2.py
@@ -0,0 +1,6 @@
+# Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
+
+linguagens: list = ["Python", "Java", "C++", "JavaScript"]
+linguagens.remove("C++")
+linguagens.append("Ruby")
+print(linguagens)
