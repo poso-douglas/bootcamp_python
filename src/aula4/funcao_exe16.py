8f434429b347442c580b223b553b2e8a17c7cf56
Douglas Poso <douglas.poso@gmail.com>
2024-05-07 15:38:35 -0300

    teste pre-commit

--- src/aula4/funcao_exe16.py
+++ src/aula4/funcao_exe16.py
@@ -1,6 +1,8 @@
 # Escreva uma função que receba uma lista de números e retorne a soma de todos os números
 
+
 def somar_numeros(lista_num: list) -> int:
     return sum(lista_num)
 
-print(somar_numeros([10,20,30]))
+
+print(somar_numeros([10, 20, 30]))
